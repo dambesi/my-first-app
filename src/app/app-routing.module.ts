4b5746ed4f2b4e6d3cb2cc13b3cc6871276cb5d3
dambesi <d.ambesi@avans.nl>
2022-11-10 16:11:14 +0100

    Code LW 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailComponent } from './pages/entity/detail/detail.component';
+import { EditComponent } from './pages/entity/edit/edit.component';
+import { EntityComponent } from './pages/entity/entity.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', pathMatch: 'full', component: DashboardComponent },
+  { path: 'component-a', pathMatch: 'full', component: AComponent },
+  { path: 'component-b', pathMatch: 'full', component: BComponent },
+  {
+    path: 'users',
+    component: EntityComponent,
+    children: [
+      { path: ':id', pathMatch: 'full', component: DetailComponent },
+      { path: ':id/edit', pathMatch: 'full', component: EditComponent },
+    ],
+  },
+  { path: 'register', pathMatch: 'full', component: RegisterComponent },
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: '**', redirectTo: 'dashboard' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './shared/nav/nav.component';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { EntityComponent } from './pages/entity/entity.component';
+import { ListComponent } from './pages/entity/list/list.component';
+import { DetailComponent } from './pages/entity/detail/detail.component';
+import { EditComponent } from './pages/entity/edit/edit.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AComponent,
+    BComponent,
+    EntityComponent,
+    ListComponent,
+    DetailComponent,
+    EditComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
